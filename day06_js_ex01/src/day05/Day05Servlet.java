db9ad2dbfcc4046e79002f8b5b409400ee2cf114
yuz <chyuz9090@gmail.com>
2022-08-19 17:56:44 +0900

    서블릿&js나머지

---
+++ day06_js_ex01/src/day05/Day05Servlet.java
@@ -0,0 +1,43 @@
+package day05;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/data.do")
+public class Day05Servlet extends HttpServlet {
+   private static final long serialVersionUID = 1L;
+
+   protected void doProcess(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      req.setCharacterEncoding("UTF-8");
+      resp.setCharacterEncoding("UTF-8");
+      resp.setContentType("text/html; charset=UTF-8");
+      
+      PrintWriter out = resp.getWriter();
+      int id = Integer.parseInt( req.getParameter("id") );
+      String name = req.getParameter("name");
+      String phone = req.getParameter("phone");
+      String email = req.getParameter("email");
+      
+      String jsonStr = "{\"id\":"+id+",\"name\":\""+name+"\",\"phone\":\""+phone+"\",\"email\":\""+email+"\"}";
+      
+      out.println(jsonStr);
+      
+      out.close();
+   }
+   
+   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      doProcess(req, resp);
+   }
+   
+   @Override
+   protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      doProcess(req, resp);
+   }
+
+}
